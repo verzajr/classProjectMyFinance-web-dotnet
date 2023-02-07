31187b51b20686773a56ebfb9cf534be17c724f3
verzajr <acverza.jr@gmail.com>
2023-02-07 20:24:09 -0300

    Initial commit

---
+++ myFinance-web-netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myFinance_web_netcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
